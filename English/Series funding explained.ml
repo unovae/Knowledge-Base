dd53d31a4f22227dc507fe49937a6c3ca4a93f78
abdul3532 <103254900+abdul3532@users.noreply.github.com>
2024-04-30 21:33:29 +0100

    Create Series funding explained.ml

---
+++ English/Series funding explained.ml
@@ -0,0 +1,29 @@
+# Series Funding Explained
+
+## 1. What is series funding?
+Series funding, or equity funding, is a way to raise capital for a startup or a company by going through several rounds of fundings where they exchange investments in return for equity. The startup gets the capital it needs to realize the idea, while the investors get an opportunity to get a handsome ROI on their investments if the company becomes successful. The rounds are spread out on the timeline of the progress of the company. It starts with pre-seed or seed and then continues with series A, series B until series D.
+
+## 2. Pre-Seed Funding
+Pre-Seed Funding is the very starting phase of the startup, where the only people in the company are founders themselves. This phase is focused on developing proof of concept or a MVP. At this stage, the capital is gained by family, friends, or an individual’s own savings. The fundings secured in this round are most likely to be a low amount ranging from a few thousands to 100,000 dollars.
+
+## 3. Seed Funding
+This stage is considered the first official funding round by external sources. It is aimed to make the startups stand on their own and prepare them for growth, like a seed is prepared to bloom into a flower. Capital at this stage can be still raised by family or friends but, most common investor type at this phase is Angel investors. Unfortunately, according to this [report](https://spdload.com/blog/startup-success-rate/#:~:text=About%2060%25%20of%20companies%20that,Copy.ai.), 60 percent of the startups do not make it past this phase as they do not get traction before the money runs out.
+
+## 4. Series A 
+Once a start-up reaches the point where it gets some kind of traction and is ready to raise their performance to the next level, they go for series A funding. At this stage, the start-up is expected to construct a business model so that they use the money raised to generate revenue. The most common type of investor at this stage is Venture capital firms, where they invest 2-15 million dollars in exchange for 15 to 25% of equity in the company. If a company does not make it to series A, they go for equity crowdfunding instead.
+
+## 5. Series B
+When a start-up has found a product/market fit, and needs now to scale up its business, that is where series B fundings take place. The whole effort at this point is to expand your business, increasing its customer base exponentially to generate more revenue. In most cases, to expand your business you need to expand the team first, so that it can handle the growth accordingly. This is the point where the founders have to stop working as a CEO, CFO, customer support officer, and a janitor all at once. Therefore, raising enough capital to give competitive salaries to attract talent is necessary. This round of funding also usually comes from Venture capital firms, most likely the ones that led the previous rounds. Typically, the investment received in series B is around 7-35 million, depending on the valuation given.
+
+## 6. Series C
+Reaching this round means that the company is now stable and is generating decent revenue. Now, they are looking to expand their business to new markets, maybe expanding their product or service catalog as well. The most common goal at this point is to increase the company valuation before going for an acquisition or an IPO (Initial Public Offering). This is where Private equity firms and investment bankers come into the investment scene. They want to invest large amounts, taking minimal risk, and reassuring that the company remains the market leader. The typical amount raised in this round is 25 to 60 million, again depending on needs and valuation. Usually, this is the last funding round that takes place, but some even go further to series D or maybe even series E.
+
+## 7. Series D&E
+Series D occurs in two scenarios, one positive and the other negative. The positive scenario is that the company is that they found the potential to further increase the company valuation before the acquisition or IPO and want to give a final push to increase the valuation. The negative scenario is that the company has failed to accomplish the goals or fulfill the expectations and wants to further raise money to deliver them. This usually is called as a “Down round” where the company raises money at a lower valuation. The devaluation can have adverse effects on the company’s credibility and morale. Series E is almost identical to series D. Approaching it can be due to the same reasons, and it also carries similar outcomes.
+
+## 8. Other types of fundings
+Series funding may be the most popular way to raise money, but it certainly is not the only way. Some of the Alternatives are:
+- **Crowdfunding:** Capital can be raised by the “Crowd” in return for equity or exclusive perks such as discounts and exclusive access.
+- **Bootstrapping:** This method keeps the ownership exclusive to the founders, who themselves act as investors for their company.
+- **Loans:** Governments give loans to starting businesses to help foster the innovative ecosystem and give a positive impact on the economy.
+- **Grants:** Governments or some private institution give certain grants or fundings to companies that work with mutual interests.
